08ffda68964eb5cf944378d0a620cd100b2fef93
NesterovIA_ABs-525 <ivannesterov200514@gmail.com>
2026-03-05 11:25:57 +0700

    CheckMyCode

---
+++ NesterovABs525Triangle/NesterovABs525Triangle.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int a, b, c, halfP, P, sGer;
+
+    cout << "Введите стороны треугольника a, b, c: \n";
+    cin >> a >> b >> c;
+
+    if (a + b <= c || a + c <= b || b + c <= a) cout << "\nТреугольника не существует.\n";
+    else {
+        P = a + b + c;
+        halfP = P / 2;
+        sGer = sqrt(P * (P - a) * (P - b) * (P - c));
+        cout << "\nПериметр: " << P << "\n";
+        cout << "Прощадь по формуле Герона: " << sGer << "\n";
+
+        if (a == b || b == c || a == c) cout << "Треугольник равнобедренный\n";
+        else cout << "Треугольник не равнобедренный.\n";
+    }
+}
