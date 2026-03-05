46098ce87c6b151c1062eaeb07eaa41a885a2dc8
REDdragonz33 <vasilbariev@gmail.com>
2026-03-06 05:21:38 +0700

    Добавление файлов перед переходом с ветки на ветку

---
+++ geometry.h
@@ -0,0 +1,9 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+void Rectangle();
+void Triangle();
+void Trapezoid();
+void Circle();
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "geometry.h"
+
+using namespace std;
+
+int main(){
+	int choice;
+	
+	cout << "Выберите фигуру:" << endl;
+	cout << "1. Прямоугольник" << endl;
+	cout << "2. Треугольник" << endl;
+	cout << "3. Трапеция" << endl;
+	cout << "4. Окружность" << endl;
+	cout << "0. Выход" << endl;
+	
+	cin >> choice;
+	
+	switch(choice){
+	case 1:
+		Rectangle();
+		break;
+	case 2:
+		Triangle();
+		break;
+	case 3:
+		Trapezoid();
+		break;
+	case 4:
+		Circle();
+		break;
+	case 0:
+		cout << "Программа завершена"<< endl;
+		break;
+}
+
+return 0;
+}
