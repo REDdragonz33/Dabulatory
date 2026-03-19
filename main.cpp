724c37d8d46fb36dcf4a410dec9bda1eb524ff6e
Momoka90 <ounular@gmail.com>
2026-03-19 21:08:03 +0700

    проверка на отрицательные числа

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <clocale>
+using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
-    float a, b, h, c, d;
-    std::cout << "Введите основания: ";
-    std::cin >> a >> b;
-    std::cout << "Введите боковые стороны: ";
-    std::cin >> c >> d;
-    std::cout << "Введите высоту: ";
-    std::cin >> h;
-
-    float ploshad = (a + b) / 2.0 * h; 
-    float srLinia = (a + b) / 2.0;
-    float perimetr = a + b + c + d; 
-    std::cout << "Площадь: " << ploshad << std::endl;
-    std::cout << "Средняя линия: " << srLinia << std::endl;
-    std::cout << "Периметр: " << perimetr << std::endl;
+    double a, b, c, d, h;
+    cout << "Введите основания: ";
+    cin >> a >> b;
+    cout << "Введите боковые стороны: ";
+    cin >> c >> d;
+    cout << "Введите высоту: ";
+    cin >> h;
+	
+    if (a <= 0 || b <= 0 || c <= 0 || d <= 0 || h <= 0){
+    cout << "Значения должны быть больше нуля" << endl;
+    return 0;
+    }
+    double ploshad = (a + b) / 2.0 * h; 
+    double srLinia = (a + b) / 2.0;
+    double perimetr = a + b + c + d; 
+    cout << "Площадь: " << ploshad << endl;
+    cout << "Средняя линия: " << srLinia << endl;
+    cout << "Периметр: " << perimetr << endl;
 
     return 0;
 }
